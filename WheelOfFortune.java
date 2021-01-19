82d1b4b05d74f16f187b8a8930ac165f9de028d2
Paul Lee <contact@paullee.dev>
2021-01-19 14:19:43 -0500

    bootstrap project

---
+++ WheelOfFortune.java
@@ -0,0 +1,29 @@
+/*
+ * Name: Paul Lee and Peter Ye
+ * Teacher: Ms Krasteva
+ * Date: 2021 Jan 19 14:17
+ * Assignment: Wheel of Fortune ISP
+ */
+
+/*
+ * Program Purpose: Recreate the Wheel of Fortune game in Ready to Program
+ * Variables:
+ * - Console c: reference to console
+ */
+
+import java.awt.*;
+import hsa.Console;
+import java.io.*;
+
+public class WheelOfFortune {
+    Console c;
+
+    // Class Constructor
+    public WheelOfFortune() {
+        c = new Console();
+    }
+
+    public static void main(String[] args) {
+        WheelOfFortune e = new WheelOfFortune();
+    }
+}
