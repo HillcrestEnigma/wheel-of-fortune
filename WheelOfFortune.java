a690edad6e4d1a18ec777c2b3069297a9456aa7d
Paul Lee <contact@paullee.dev>
2021-01-19 14:38:12 -0500

    rename c to console

--- WheelOfFortune.java
+++ WheelOfFortune.java
@@ -8,7 +8,7 @@
 /*
  * Program Purpose: Recreate the Wheel of Fortune game in Ready to Program
  * Variables:
- * - Console c: reference to console
+ * - Console console: reference to console
  */
 
 import java.awt.*;
@@ -16,14 +16,25 @@ import hsa.Console;
 import java.io.*;
 
 public class WheelOfFortune {
-    Console c;
+    Console console;
 
     // Class Constructor
     public WheelOfFortune() {
-        c = new Console();
+        console = new Console();
+    }
+
+    private void drawLetterGrid(int x,int y, int squareSize, char[][] grid) {
     }
 
     public static void main(String[] args) {
-        WheelOfFortune e = new WheelOfFortune();
+        WheelOfFortune game = new WheelOfFortune();
+        try {
+            while (true) {    
+                game.newRound();
+            }
+        }
+        catch(UserExitException e) { // UserExitException is thrown when user wants to exit
+            game.goodbye();
+        }
     }
 }
