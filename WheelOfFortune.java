9bfae384c75be494737440a53874d129f529d202
Peter Ye <yaxollum@gmail.com>
2021-01-21 11:27:45 -0500

    finished spinning wheel

--- WheelOfFortune.java
+++ WheelOfFortune.java
@@ -12,6 +12,7 @@
  */
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import hsa.Console;
 import java.io.*;
 
@@ -19,6 +20,14 @@ public class WheelOfFortune {
     Console console;
     final int CONSOLE_WIDTH=1024;
     final int CONSOLE_HEIGHT=768;
+
+    final int LOSE_TURN=-1;
+    final int BANKRUPT=-2;
+
+    final int[] WHEEL_VALUES={
+        LOSE_TURN,2500,700,600,550,
+        BANKRUPT,600,550,500,800,
+        LOSE_TURN,800,500,900,500};
 
     // Class Constructor
     public WheelOfFortune() {
@@ -37,6 +46,106 @@ public class WheelOfFortune {
         console.println(console.getWidth());
         console.println(console.getHeight());
         */
+
+        for(double ang=0;;ang+=0.01)
+        {
+            drawWheel(400,400,200,ang);
+            try
+            {
+            Thread.sleep(30);
+
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private void drawWheelBaseToGraphics(Graphics2D graphics,Color[] WHEEL_COLORS,int radius, double angle)
+    {
+        final int SLICES=WHEEL_VALUES.length;
+        int linesToDraw=(int)(2*Math.PI*radius);
+        int linesPerSlice=linesToDraw/SLICES+1;
+
+        int xPrev=(int)(Math.cos(angle)*radius)+radius;
+        int yPrev=(int)(Math.sin(angle)*radius)+radius;
+
+        for(int i=0;i<linesToDraw;++i)
+        {
+            double lineAngle=angle+2*Math.PI*i/linesToDraw;
+            graphics.setColor(WHEEL_COLORS[i/linesPerSlice]);
+            if(i%linesPerSlice<3)
+            {
+                graphics.setColor(Color.BLACK);
+            }
+            int xNew=(int)(Math.cos(lineAngle)*radius)+radius;
+            int yNew=(int)(Math.sin(lineAngle)*radius)+radius;
+            graphics.fillPolygon(new int[]{radius,xPrev,xNew},new int[]{radius,yPrev,yNew},3);
+            xPrev=xNew;
+            yPrev=yNew;
+        }
+        graphics.setColor(Color.BLACK);
+        for(int i=0;i<linesToDraw;++i)
+        {
+            double lineAngle=angle+2*Math.PI*i/linesToDraw;
+            int xBorder=(int)(Math.cos(lineAngle)*(radius-1))+radius;
+            int yBorder=(int)(Math.sin(lineAngle)*(radius-1))+radius;
+            graphics.fillOval(xBorder-2,yBorder-2,4,4);
+        }
+
+    }
+    private void drawWheelTextToGraphics(Graphics2D graphics,int radius, double angle)
+    {
+        graphics.rotate(angle-Math.PI*0.3,radius,radius);
+        graphics.setFont(new Font("Arial",Font.BOLD,radius/12));
+        final int SLICES=WHEEL_VALUES.length;
+        for(int i=0;i<SLICES;++i)
+        {
+            if(WHEEL_VALUES[i]==BANKRUPT)
+            {
+                graphics.setColor(Color.WHITE);
+            }
+            else
+            {
+                graphics.setColor(Color.BLACK);
+            }
+            String sliceStr;
+            if(WHEEL_VALUES[i]==BANKRUPT)
+            {
+                sliceStr="BANKRUPT";
+            }
+            else if(WHEEL_VALUES[i]==LOSE_TURN)
+            {
+                sliceStr="LOSE A TURN";
+            }
+            else
+            {
+                sliceStr="$"+WHEEL_VALUES[i];
+            }
+            graphics.drawString(sliceStr,radius/8,radius);
+            graphics.rotate(2*Math.PI/SLICES,radius,radius);
+        }
+
+    }
+
+    private void drawWheel(int x,int y,int radius,double angle)
+    {
+        final Color[] WHEEL_COLORS={
+            Color.LIGHT_GRAY,Color.ORANGE,Color.GREEN,Color.YELLOW,Color.PINK,
+            Color.LIGHT_GRAY,Color.CYAN,Color.RED,Color.YELLOW,Color.ORANGE,
+            Color.BLACK,Color.CYAN,Color.GREEN,Color.RED,Color.PINK
+        };
+        
+        final int SLICES=WHEEL_VALUES.length;
+
+        BufferedImage bufferedImage=new BufferedImage(radius*2,radius*2,BufferedImage.TYPE_INT_ARGB);
+        Graphics2D graphics=(Graphics2D)bufferedImage.getGraphics();
+        drawWheelBaseToGraphics(graphics,WHEEL_COLORS, radius, angle);
+        drawWheelTextToGraphics(graphics,radius,angle);
+
+        console.drawImage(bufferedImage,x-radius,y-radius,null);
     }
 
     private void drawLetterGrid(int x,int y, int squareSize, char[][] grid) {
